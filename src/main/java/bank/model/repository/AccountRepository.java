f6ac0ee1e4b5907c715d4e3842239ce32630fde4
Dmytro Melnyk <dimamel28@gmail.com>
2024-04-03 22:52:22 +0300

    Changed repo name

--- src/main/java/bank/model/repository/AccountRepository.java
+++ src/main/java/bank/model/repository/AccountRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface AccountRepo extends JpaRepository<BankAccount, Long> {
+public interface AccountRepository extends JpaRepository<BankAccount, Long> {
 
     Optional<BankAccount> findBankAccountByUserId(Long userId);
 
