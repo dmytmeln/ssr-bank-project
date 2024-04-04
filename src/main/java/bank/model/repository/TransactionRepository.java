b63d4bcd4ceb17bae44a6b4800bee3efcd2dc4d2
Dmytro Melnyk <dimamel28@gmail.com>
2024-04-04 15:26:28 +0300

    Renamed method findTransactionHistoriesByBankAccountId -> findTransactionsByBankAccountId

--- src/main/java/bank/model/repository/TransactionRepository.java
+++ src/main/java/bank/model/repository/TransactionRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-    List<Transaction> findTransactionHistoriesByBankAccountId(Long bankAccountId);
+    List<Transaction> findTransactionsByBankAccountId(Long bankAccountId);
 
 }
