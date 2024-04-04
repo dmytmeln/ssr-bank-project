d49565ac24fa7f8bd6e7c340ef81f053424a8a53
Dmytro Melnyk <dimamel28@gmail.com>
2024-04-04 23:27:13 +0300

    Changed filter logic

--- src/main/java/bank/filters/UnauthorizedUserFilter.java
+++ src/main/java/bank/filters/UnauthorizedUserFilter.java
@@ -1,41 +1,22 @@
 package bank.filters;
 
-// class to filter unauthorized users that trying to access profile or transaction history
-
+import bank.model.domain.User;
+import org.springframework.web.filter.OncePerRequestFilter;
 import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter(servletNames = {"UserServlet", "TransactionsServlet", "BankServlet"})
-public class UnauthorizedUserFilter implements Filter {
-
+public class UnauthorizedUserFilter extends OncePerRequestFilter {
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        Long id  = (Long) request.getSession().getAttribute("id");
-        if (id == null) {
-            HttpServletResponse response = (HttpServletResponse) servletResponse;
-            response.sendRedirect("/auth");
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        User user = (User) httpServletRequest.getSession().getAttribute("user");
+        if (user == null) {
+            httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/auth");
             return;
         }
 
-        filterChain.doFilter(servletRequest, servletResponse);
-
-
-
-    }
-
-    @Override
-    public void destroy() {
-
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
     }
 
 }
