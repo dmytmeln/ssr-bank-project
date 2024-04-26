b9ac2cf934b1d7248e8ca5792a82f06f36b17b7d
Dmytro Melnyk <dimamel28@gmail.com>
2024-04-26 22:36:39 +0300

    Added role in INSERT INTO users

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO users(firstname, lastname, password, email, phone_number)
-VALUES ('Dmytro', 'Melnyk', 'asAS12!@', 'dimamel28@gmail.com', '380984035791');
+INSERT INTO users(firstname, lastname, password, email, phone_number, role)
+VALUES ('Dmytro', 'Melnyk', 'asAS12!@', 'dimamel28@gmail.com', '380984035791', 'USER');
 
 INSERT INTO bank_accounts(id, balance, user_id)
 VALUES (1, 1000, 1);
